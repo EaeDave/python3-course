ebfb7716e8ab55a945f7aa7c67f6177d9d1b825a
Zaphryn <eudaverdgs@gmail.com>
2025-05-20 20:49:10 -0300

    Tipo str (string)

---
+++ aula0003.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)  # Isso seria um número inteiro literal
+
+# Aspas simples
+print('Isso é uma string (texto)')
+
+# Aspas duplas
+print("Isso também é uma string (texto)")
+
+
+# Escape
+print("Colocando \"Aspas\" com o caractere de escape")  # Esse caractere faz o interpretador ignorar a lógica por trás.
+
+# r
+print(r"David \"Rodrigues\"")
