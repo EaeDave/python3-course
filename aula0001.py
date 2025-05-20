8729de83568852a579eadf8970c83ec55a90a9ea
Zaphryn <eudaverdgs@gmail.com>
2025-05-20 20:26:17 -0300

    Docstrings como comentários de múltiplas linhas

--- aula0001.py
+++ aula0001.py
@@ -1,3 +1,12 @@
 # Isso é um comentário de código e o interpretador do Python não executará.
 print('Apenas esse comentário foi impresso no output.')  # print é uma função
 # que exibe algo na tela.
+
+"""
+Docstrings (Não é um comentário de múltiplas linhas, o interpretador do Python
+lê, porém não exibe nada no output, pois serve para outra coisa.)
+"""
+
+'''
+Também é possível usar Docstrings com aspas simples
+'''
