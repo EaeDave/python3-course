2c0cf59c658c106a65a1249aa17012d293f4848c
Zaphryn <eudaverdgs@gmail.com>
2025-05-21 19:12:03 -0300

    Exercício: Cálculo de IMC + Ellipsis

---
+++ aula0012.py
@@ -0,0 +1,12 @@
+nome = 'David Rodrigues'
+altura = 1.70
+peso = 71
+# imc = ...  # Os três pontos representa Ellipsis, um valor que ainda será preenchido e o Python ignora.
+imc = peso / (altura * altura)
+
+# David Rodrigues tem 1.70 de altura,
+# pesa 71 quilos e seu IMC é
+# 24.57
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
