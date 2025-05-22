91c0304e397f5608bd1ced23eda9a38557e14ffb
Zaphryn <eudaverdgs@gmail.com>
2025-05-22 19:49:01 -0300

    if, elif e else: entendendo o fluxo do interpretador em condicionais

---
+++ aula0017.py
@@ -0,0 +1,11 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao = False
+
+if condicao:
+    print('Este código só será executado caso a condição seja True.')
+else:
+    print('Este código só será executado caso a condição seja False.')
+
+print('Fora do bloco.')  # Será executado independentemente, pois não está dentro do bloco condicional.
