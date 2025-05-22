2848d68c38b58fa35c6f9a64107e8572e0137569
Zaphryn <eudaverdgs@gmail.com>
2025-05-22 19:52:57 -0300

    .gitignore na pasta .vscode + Debugger do VSCode

---
+++ aula0018.py
@@ -0,0 +1,13 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao = False
+
+if condicao:
+    print('Este código só será executado caso a condição seja True.')
+else:
+    print('Este código só será executado caso a condição seja False.')
+
+print('Fora do bloco.')  # Será executado independentemente, pois não está dentro do bloco condicional.
+
+# Nesta aula foi apresentado o Debugger do VSCode, breakpoints e análise de código.
