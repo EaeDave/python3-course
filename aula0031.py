28c98a17468bfd78895b7f0ff9f25f121c04945c
EaeDave <eudaverdgs@gmail.com>
2025-05-25 16:49:26 -0300

    Flags, is, is not e None

--- aula0031.py
+++ aula0031.py
@@ -5,14 +5,20 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
 
-v1 = 'a'
-v2 = 'a'
-print(id(v1))  # Como ambas as variáveis possuem valores iguais
-print(id(v2))  # Ambas apontarão para o mesmo local na memória
+# v1 = 'a'
+# v2 = 'a'
+# print(id(v1))  # Como ambas as variáveis possuem valores iguais
+# print(id(v2))  # Ambas apontarão para o mesmo local na memória
 
-# condicao = False
+condicao = False
+passou_no_if = None
 
-# if condicao:
-#   print('Faça algo')
-# else:
-#   print('Não faça algo')
+if condicao:
+  passou_no_if = True
+  print('Faça algo')
+else:
+  print('Não faça algo')
+
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
