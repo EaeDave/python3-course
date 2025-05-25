e566324042ee2face137988133cfb014e32ae59b
EaeDave <eudaverdgs@gmail.com>
2025-05-25 16:27:11 -0300

    id - A identidade do valor que está na memória

---
+++ aula0031.py
@@ -0,0 +1,18 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))  # Como ambas as variáveis possuem valores iguais
+print(id(v2))  # Ambas apontarão para o mesmo local na memória
+
+# condicao = False
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#   print('Não faça algo')
