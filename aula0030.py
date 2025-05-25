ee26bc37b784da0030b56c0667ded5aa3252224e
EaeDave <eudaverdgs@gmail.com>
2025-05-25 16:13:42 -0300

    Parte 2: Variáveis, constantes e complexidade de código

--- aula0030.py
+++ aula0030.py
@@ -5,8 +5,15 @@ Muitas condições no mesmo if (ruim)
 """
 
 velocidade = 61  # velocidade atual do carro
-local_carro = 90  # local em que o carro está na estrada
+local_carro = 100  # local em que o carro está na estrada
 
 RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pega
+
+velocidade_carro_passou_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= LOCAL_1 - RADAR_RANGE and local_carro <= LOCAL_1 + RADAR_RANGE
+
+if velocidade_carro_passou_radar_1 and carro_passou_radar_1:
+    print('Passou da velocidade do radar 1.')
+    print('Carro multado.')
