4d1a40ba68ffd1bb341f4d222005b55dc20d279d
EaeDave <eudaverdgs@gmail.com>
2025-06-02 21:34:39 -0300

    Tipos built-in, documentação, tipo imutável, métodos de string

---
+++ aula0033.py
@@ -0,0 +1,12 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'David Rodrigues'
+
+outra_variavel = string
+print(string[3])  # i
+# string[3] = 'O'  Não é possível mudar
+
+print(string.upper())  # Utilizando um método do objeto str
