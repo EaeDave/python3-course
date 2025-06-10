d737cc32b76c74d1a70d686f5246a278863b4d6a
EaeDave <eudaverdgs@gmail.com>
2025-06-10 19:47:51 -0300

    Operadores de atribuição com operadores aritméticos

---
+++ aula0036.py
@@ -0,0 +1,13 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador+= 1  # Usando o operador de atribuição += (o valor antigo + um outro valor)
+
+
+print('Acabou')
